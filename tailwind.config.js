641d15ad76659b653a897de7e30de455cd108e44
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-01-11 20:27:47 +0900

    💡

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		"src/pages/**/*.{js,ts,jsx,tsx}",
+		"src/components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
